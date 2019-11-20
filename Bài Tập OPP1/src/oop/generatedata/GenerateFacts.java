4dc98c46e57702824474b54bce5a15960dcfafe5
akirakyohei <42181764+akirakyohei@users.noreply.github.com>
2019-11-20 12:18:47 +0700

    update 1

--- Bài Tập OPP1/src/oop/generatedata/GenerateFacts.java
+++ Bài Tập OPP1/src/oop/generatedata/GenerateFacts.java
@@ -8,7 +8,7 @@ import oop.beans.Fact;
 
 public class GenerateFacts {
 
-	public static ArrayList<Fact> generate(String fact,ArrayList<? extends Entity> objects,ArrayList<? extends Entity> subjects,int soLuong) {
+	public static  ArrayList<Fact> generate(String fact,ArrayList<? extends Entity> objects,ArrayList<? extends Entity> subjects,int soLuong) {
 		ArrayList<Fact> facts=new ArrayList<>();
 		for(int i=0;i<soLuong;i++) {
 			Random ran=new Random();
